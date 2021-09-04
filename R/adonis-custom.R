c974eb2bf11bfb1c3250b2891e5ff7de8fbcb164
Kyle Bittinger <kylebittinger@gmail.com>
2021-09-04 14:36:18 -0400

    Add adonisplus and adonispost functions

--- R/adonis-custom.R
+++ R/adonis-custom.R
@@ -1,3 +1,140 @@
+#' Permutational multivariate analysis of variance, plus
+#'
+#' @param data Data to use in the test
+#' @param distmat Distance matrix, either a matrix or an object of class
+#'   \code{dist} The distance matrix will automatically be filtered and
+#'   re-arranged to match the rows of \code{data}
+#' @param formula Model formula. The LHS must be "distmat ~". The formula can
+#'   either be a literal formula or a string that can be converted into a
+#'   formula.
+#' @param sample_id_var Variable in \code{data} that defines the sample IDs,
+#'   i.e. the identifiers that correspond to each item in the distance matrix
+#' @param rep_meas_var Variable in \code{data} that indicates the repeated
+#'   measures in the experiment, typically a subject ID or cage ID
+#' @param shuffle Named character vector that specifies how to carry out
+#'   restricted permutations for variables in \code{data}. Names should
+#'   correspond to variables in \code{data}. Values should be either
+#'   \code{"between"} or \code{"within"}, meaning that values should be
+#'   shuffled between or within levels of \code{rep_meas_var}. See Details for
+#'   more info
+#' @param permutations Number of permutations
+#' @param seed Random seed, set just before the initial call to
+#'   \code{vegan::adonis()}
+#' @return The results from \code{vegan::adonis()} in tidy format
+#' @details
+#' A typical experimental design has subjects in a few groups sampled
+#' repeatedly over a few time points. If the variable denoting the group is
+#' \code{study_group} and the variable denoting the time point is
+#' \code{time_point}, then the \code{shuffle} argument would be
+#' \code{c(study_group = "between", time_point = "within")}. During the
+#' permutation stage, the values of the study group will be shuffled between
+#' subjects, preserving the value within each subject. Conversely, the values
+#' of the time point will be shuffled only within each subject.
+#' @export
+adonisplus <- function(data, distmat, formula, sample_id_var = SampleID,
+                       rep_meas_var = subject_id, shuffle = NULL,
+                       permutations = 999, seed = 42) {
+  sample_ids <- data %>%
+    dplyr::pull({{ sample_id_var }}) %>%
+    as.character()
+  distmat <- usedist::dist_subset(distmat, sample_ids)
+  formula <- as.formula(formula)
+  # TODO: determine if LHS is equal to "distmat ~" and stop if not
+
+  set.seed(seed)
+  a_observed <- vegan::adonis(
+    formula = formula, data = data, permutations = permutations)
+  result <- tidy.adonis(a_observed)
+
+  if (!is.null(shuffle)) {
+    rep_meas_vals <- data %>%
+      dplyr::pull({{ rep_meas_var }})
+
+    # Here, vars is the variables that will undergo custom permutations
+    vars <- names(shuffle)
+    # TODO: handle errors from missing variable names
+    # Matrix of variables by terms
+    vars_by_terms <- attr(terms(formula), "factors")
+    nterms <- length(colnames(vars_by_terms))
+    # TODO: handle errors due to variable names not matching
+    # Keep only the variables that we will shuffle manually
+    vars_idx <- match(vars, rownames(vars_by_terms))
+    # Need drop = FALSE or R converts to a vector when nterms == 1
+    vars_by_terms <- vars_by_terms[vars_idx, , drop = FALSE]
+    term_idx <- which(colSums(vars_by_terms) > 0)
+
+    f_observed <- result$statistic[term_idx]
+    fs_permuted <- replicate(permutations, {
+      trial_data <- data
+      for (var in vars) {
+        method <- shuffle[var]
+        shuffle_functions <- list(
+          between = shuffle_between_groups,
+          within = shuffle_within_groups)
+        # Use match.arg in case someone writes "bet" instead of "between"
+        method <- match.arg(method, names(shuffle_functions))
+        fcn <- shuffle_functions[[method]]
+        old_vals <- trial_data[[var]]
+        new_vals <- fcn(old_vals, rep_meas_vals)
+        trial_data[[var]] <- new_vals
+      }
+      trial_a <- vegan::adonis(formula, trial_data, permutations = 4)
+      trial_result <- tidy.adonis(trial_a)
+      trial_result$statistic[term_idx]
+    })
+
+    if (nterms > 1) {
+      fs_greater <- sweep(cbind(f_observed, fs_permuted), 1, f_observed, `>=`)
+      p_permuted <- apply(fs_greater, 1, function (x) sum(x) / length(x))
+    } else {
+      fs_greater <- c(f_observed, fs_permuted) >= f_observed
+      p_permuted <- sum(fs_greater) / length(fs_greater)
+    }
+    result$p.value[term_idx] <- p_permuted
+  }
+  result
+}
+
+#' Post-hoc tests for permutational multivariate analysis of variance
+#'
+#' @param data Data to use in the test
+#' @param ... Additional arguments passed to \code{adonisplus()}
+#' @param which Variable on which to perform post-hoc comparisons
+#' @param alpha The threshold for rejecting the null hypothesis in the main
+#'   comparison
+#' @return The results in tidy format
+#' @export
+adonispost <- function(data, ..., which = study_group, alpha = 0.05) {
+  var_name <- rlang::as_name(rlang::ensym(which))
+
+  result_main <- adonisplus(data, ...) %>%
+    dplyr::mutate(comparison = paste("All", var_name)) %>%
+    dplyr::select(term, comparison, everything()) %>%
+    dplyr::filter(!(term %in% c("Residuals", "Total")))
+
+  if (result_main$p.value > alpha) {
+    return(result_main)
+  }
+
+  var_levels <- data %>%
+    dplyr::pull({{ which }}) %>%
+    as.factor() %>%
+    levels()
+  pairs <- combn(var_levels, 2, simplify = FALSE)
+
+  make_pairwise_comparison <- function (pair) {
+    pair_data <- data %>%
+      dplyr::filter({{ which }} %in% pair)
+    adonisplus(pair_data, ...) %>%
+        dplyr::mutate(comparison = paste(pair, collapse = " - ")) %>%
+        dplyr::select(term, comparison, everything()) %>%
+        dplyr::filter(!(term %in% c("Residuals", "Total")))
+  }
+  result_posthoc <- lapply(pairs, make_pairwise_comparison) %>%
+    dplyr::bind_rows()
+  dplyr::bind_rows(result_main, result_posthoc)
+}
+
 #' Permutational multivariate analysis of variance for repeated measures
 #'
 #' @param data Data to use in the test
@@ -111,7 +248,7 @@ adonis_run <- function(data, distmat,
                        seed = 42) {
 
   sample_ids <- as.character(dplyr::pull(data, {{ sample_id_var }}))
-  
+
   strata_ids <- c()
   if (!rlang::quo_is_null(rlang::enquo(rep_meas_var))) {
     strata_ids <- as.character(dplyr::pull(data, {{ rep_meas_var }}))
@@ -126,9 +263,9 @@ adonis_run <- function(data, distmat,
 
   set.seed(seed)
   if(is.null(strata_ids)) {
-    tidy.adonis(adonis(adonis_formula, data=data, permutations=permutations))
+    tidy.adonis(vegan::adonis(adonis_formula, data=data, permutations=permutations))
   } else {
-    tidy.adonis(adonis(adonis_formula, data=data, permutations=permutations, strata=strata_ids))
+    tidy.adonis(vegan::adonis(adonis_formula, data=data, permutations=permutations, strata=strata_ids))
   }
 }
 
--- tests/testthat/test-adonis-custom.R
+++ tests/testthat/test-adonis-custom.R
@@ -3,7 +3,8 @@ example_data <- tibble::tibble(
     "C1", "C2", "C3", "C4", "C5", "P1", "P2", "P3", "P4", "P5", "P6"),
   time_point = c(rep("C", 5), rep("P", 6)),
   SubjectID = paste0("S", c(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6)),
-  study_group = rep(c("G1", "G2", "G1", "G2"), c(3, 2, 3, 3)))
+  study_group = rep(c("G1", "G2", "G1", "G2"), c(3, 2, 3, 3)),
+  age = c(1.2, 1.1, 1.3, 1.0, 1.4, 1.2, 1.1, 1.3, 1.0, 1.4, 1.5))
 
 
 example_data2 <- tibble::tibble(
@@ -12,6 +13,14 @@ example_data2 <- tibble::tibble(
   time_point = c(rep("C", 5), rep("P", 6)),
   SubjectID = paste0("S", c(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6)),
   study_group = rep(c("G1", "G2", "G3", "G1", "G2", "G3"), c(2,2,1, 1, 2, 3)))
+
+example_data3 <- tibble::tibble(
+  SampleID = c(
+    "C1", "C2", "C3", "C4", "C5", "P1", "P2", "P3", "P4", "P5", "P6"),
+  time_point = c(rep("C", 5), rep("P", 6)),
+  SubjectID = paste0("S", c(1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6)),
+  study_group = rep(c("G1", "G2", "G3", "G1", "G2", "G3"), c(2,2,1, 2, 2,2)))
+
 
 example_dist <- structure(
   c(
@@ -24,109 +33,182 @@ example_dist <- structure(
   Size = 11L, call = quote(as.dist.default(m = cp_dist)), class = "dist",
   Diag = FALSE, Upper = FALSE)
 
-test_that("adonis_repeated_measures produces expected result", {
+result_study_group_unrestricted <- tibble::tibble(
+  term = c("study_group", "Residuals", "Total"),
+  df = c(1, 9, 10),
+  sumsq = c(0.1412367727273, 1.4724405000000, 1.6136772727273),
+  meansq = c(0.1412367727273, 0.1636045000000, NA),
+  statistic = c(0.8632817112443, NA, NA),
+  r.squared = c(0.087524795146, 0.912475204854, 1),
+  p.value = c(0.635, NA, NA))
+
+
+result_2way_restricted <- tibble::tibble(
+  term = c(
+    "study_group", "time_point", "study_group:time_point",
+    "Residuals", "Total"),
+  df = c(1, 1, 1, 7, 10),
+  sumsq = c(
+    0.141236772727273, 0.2546555, 0.161871833333333,
+    1.05591316666667, 1.61367727272727),
+  meansq = c(
+    0.141236772727273, 0.2546555, 0.161871833333333,
+    0.150844738095238, NA),
+  statistic = c(
+    0.936305598131642, 1.68819610956015, 1.07310228634646, NA, NA),
+  r.squared = c(
+    0.0875247951460386, 0.157810675222318, 0.100312395835974,
+    0.654352133795669, 1),
+  p.value = c(0.4, 0.1, 0.3, NA, NA))
+
+result_poshoc_unrestricted <- tibble::tibble(
+  term = rep(c("study_group", "Residuals", "Total"), 4),
+  df = c(c(2, 8, 10), rep(c(1,5,6), 2), c(1,6,7)),
+  sumsq = c(
+    0.322740606060606, 1.29093666666667, 1.61367727272727, 0.154497726190476,
+    1.03401541666667, 1.18851314285714, 0.125784226190476, 0.621541916666667,
+    0.747326142857143, 0.198521625, 0.926316, 1.124837625),
+  meansq = c(
+    0.161370303030303, 0.161367083333333, NA, 0.154497726190476,
+    0.206803083333333, NA, 0.125784226190476, 0.124308383333333,
+    NA, 0.198521625, 0.154386, NA),
+  statistic = c(
+    1.00001995262543, NA, NA, 0.747076512111044, NA, NA, 1.01187243223319,
+    NA, NA, 1.28587841514127, NA, NA),
+  r.squared = c(
+    0.200003192407329, 0.799996807592671, 1, 0.129992442337717,
+    0.870007557662283, 1, 0.168312359192445, 0.831687640807555, 1,
+    0.17648913993253, 0.82351086006747, 1),
+  p.value = c(0.8, NA, NA, 1, NA, NA, 0.4, NA, NA, 0.3, NA, NA),
+  comparison = rep(c("all", "G1 - G2", "G1 - G3", "G2 - G3"), c(3,3,3,3)))
+
+result_adonispost_unrestricted <- tibble::tibble(
+  term = c("study_group", "study_group", "study_group", "study_group"),
+  comparison = c("All study_group", "G1 - G2", "G1 - G3", "G2 - G3"),
+  df = c(2, 1, 1, 1),
+  sumsq = c(
+    0.396595356060606, 0.2534925, 0.172570726190476, 0.160944833333333),
+  meansq = c(
+    0.198297678030303, 0.2534925, 0.172570726190476, 0.160944833333333),
+  statistic = c(
+    1.30343028067264, 1.38198123911777, 0.973279580007741, 1.80003549112144),
+  r.squared = c(
+    0.2457711729374, 0.18721007197831, 0.162938895956797, 0.26470972004068),
+  p.value = c(0.154, 0.173, 0.478, 0.092))
+
+test_that("adonispost even works", {
+  observed <- adonispost(
+    example_data3, example_dist, distmat ~ study_group,
+    which = study_group, alpha = 1)
+  expect_equal(observed, result_adonispost_unrestricted)
+})
+
+test_that("adonisplus works for one group with unrestricted permutations", {
+  observed <- adonisplus(example_data, example_dist, distmat ~ study_group)
+  expect_equal(observed, result_study_group_unrestricted)
+})
+
+test_that("adonisplus works for one group with restricted permutations", {
   expected <- tibble::tibble(
-    term = c(
-      "study_group", "time_point", "study_group:time_point",
-      "Residuals", "Total"),
-    df = c(1, 1, 1, 7, 10),
-    sumsq = c(
-      0.141236772727273, 0.2546555, 0.161871833333333,
-      1.05591316666667, 1.61367727272727),
-    meansq = c(
-      0.141236772727273, 0.2546555, 0.161871833333333,
-      0.150844738095238, NA),
-    statistic = c(
-      0.936305598131642, 1.68819610956015, 1.07310228634646, NA, NA),
-    r.squared = c(
-      0.0875247951460386, 0.157810675222318, 0.100312395835974,
-      0.654352133795669, 1),
-    p.value = c(0.4, 0.1, 0.3, NA, NA))
-  observed <- adonis_repeated_measures(
-    example_data, example_dist,
-    group2 = time_point, rep_meas_var = SubjectID,
+    term = c("study_group", "Residuals", "Total"),
+    df = c(1, 9, 10),
+    sumsq = c(0.141236772727273, 1.4724405, 1.61367727272727),
+    meansq = c(0.141236772727273, 0.1636045, NA),
+    statistic = c(0.863281711244329, NA, NA),
+    r.squared = c(0.0875247951460386, 0.912475204853961, 1),
+    p.value = c(0.9, NA, NA))
+  observed <- adonisplus(
+    example_data, example_dist, distmat ~ study_group,
+    rep_meas_var = SubjectID,
+    shuffle = c(study_group = "between"),
     permutations = 9)
   expect_equal(observed, expected)
 })
 
+test_that("adonisplus works for two groups", {
+  observed <- adonisplus(
+    example_data, example_dist, distmat ~ study_group * time_point,
+    rep_meas_var = SubjectID,
+    shuffle = c(study_group = "between", time_point = "within"),
+    permutations = 9)
+  expect_equal(observed, result_2way_restricted)
+})
 
-
-test_that("adonis_run produces expected result", {
+test_that("adonisplus works for two groups with covariate", {
   expected <- tibble::tibble(
     term = c(
-      "study_group", "Residuals", "Total"),
-    df = c(1, 9, 10),
+      "age", "study_group", "time_point", "study_group:time_point",
+      "Residuals", "Total"),
+    df = c(1, 1, 1, 1, 6, 10),
     sumsq = c(
-      0.1412367727273, 1.4724405000000, 1.6136772727273),
+      0.147625953372434, 0.12677620170778, 0.270223650980392,
+      0.148155646153846, 0.920895820512821, 1.61367727272727),
     meansq = c(
-      0.1412367727273, 0.1636045000000, NA),
+      0.147625953372434, 0.12677620170778, 0.270223650980392,
+      0.148155646153846, 0.153482636752137, NA),
     statistic = c(
-      0.8632817112443, NA, NA),
+      0.961841394547052, 0.825997027354399, 1.76061381729312,
+      0.965292552232516, NA, NA),
     r.squared = c(
-      0.087524795146, 0.912475204854, 1),
-    p.value = c(0.73, NA, NA))
-  observed <- adonis_run(example_data, example_dist, "study_group", SampleID, NULL, 99, 42)
+      0.0914841869978943, 0.0785635417009472, 0.167458298847878,
+      0.0918124389912542, 0.570681533462026, 1),
+    p.value = c(0.7, 0.5, 0.1, 0.5, NA, NA))
+  observed <- adonisplus(
+    example_data, example_dist, distmat ~ age + study_group * time_point,
+    rep_meas_var = SubjectID,
+    shuffle = c(study_group = "between", time_point = "within"),
+    permutations = 9)
   expect_equal(observed, expected)
 })
 
+test_that("adonis_repeated_measures produces expected result", {
+  observed <- adonis_repeated_measures(
+    example_data, example_dist,
+    group1 = study_group, group2 = time_point,
+    rep_meas_var = SubjectID, sample_id_var = SampleID,
+    permutations = 9)
+  expect_equal(observed, result_2way_restricted)
+})
+
+test_that("adonis_run produces expected result", {
+  observed <- adonis_run(example_data, example_dist, "study_group")
+  expect_equal(observed, result_study_group_unrestricted)
+})
 
 test_that("adonis_run produces expected result using the strata term", {
   expected <- tibble::tibble(
-    term = c(
-      "study_group", "Residuals", "Total"),
+    term = c("time_point", "Residuals", "Total"),
     df = c(1, 9, 10),
-    sumsq = c(
-      0.1412367727273, 1.4724405000000, 1.6136772727273),
-    meansq = c(
-      0.1412367727273, 0.1636045000000, NA),
-    statistic = c(
-      0.8632817112443, NA, NA),
-    r.squared = c(
-      0.087524795146, 0.912475204854, 1),
-    p.value = c(0.61, NA, NA))
-  observed <- adonis_run(example_data, example_dist, "study_group", SampleID, time_point, 99, 42)
+    sumsq = c(0.246518739393939, 1.36715853333333, 1.61367727272727),
+    meansq = c(0.246518739393939, 0.151906503703704, NA),
+    statistic = c(1.62283202748698, NA, NA),
+    r.squared = c(0.152768303526577, 0.847231696473423, 1),
+    p.value = c(0.09375, NA, NA))
+  observed <- adonis_run(
+    example_data, example_dist, "time_point", SampleID, SubjectID, 99, 42)
   expect_equal(observed, expected)
 })
-
-
 
 test_that("adonis_posthoc produces expected result", {
-  expected <- tibble::tibble(
-    term = rep(c("study_group", "Residuals", "Total"), 4),
-    df = c(c(2, 8, 10), rep(c(1,5,6), 2), c(1,6,7)),
-    sumsq = c(0.322740606060606, 1.29093666666667, 1.61367727272727, 0.154497726190476, 
-              1.03401541666667, 1.18851314285714, 0.125784226190476, 0.621541916666667, 
-              0.747326142857143, 0.198521625, 0.926316, 1.124837625),
-    meansq = c(0.161370303030303, 0.161367083333333, NA, 0.154497726190476, 
-               0.206803083333333, NA, 0.125784226190476, 0.124308383333333, 
-               NA, 0.198521625, 0.154386, NA),
-    statistic = c(1.00001995262543, NA, NA, 0.747076512111044, NA, NA, 1.01187243223319, 
-                  NA, NA, 1.28587841514127, NA, NA),
-    r.squared = c(0.200003192407329, 0.799996807592671, 1, 0.129992442337717, 
-                  0.870007557662283, 1, 0.168312359192445, 0.831687640807555, 1, 
-                  0.17648913993253, 0.82351086006747, 1),
-    p.value = c(0.8, NA, NA, 1, NA, NA, 0.4, NA, NA, 0.3, NA, NA),
-    comparison = rep(c("all", "G1 - G2", "G1 - G3", "G2 - G3"), c(3,3,3,3)))
-  observed <- adonis_posthoc(example_data2, example_dist, "study_group", SampleID, NULL, study_group, 1, 9, 42)
-  expect_equal(observed, expected)
+  observed <- adonis_posthoc(
+    example_data2, example_dist, "study_group", SampleID, NULL, study_group, 1, 9, 42)
+  expect_equal(observed, result_poshoc_unrestricted)
 })
-
-
 
 test_that("adonis_posthoc produces expected result with strata", {
   expected <- tibble::tibble(
     term = rep(c("study_group", "Residuals", "Total"), 4),
     df = c(c(2, 8, 10), rep(c(1,5,6), 2), c(1,6,7)),
-    sumsq = c(0.322740606060606, 1.29093666666667, 1.61367727272727, 0.154497726190476, 
-              1.03401541666667, 1.18851314285714, 0.125784226190476, 0.621541916666667, 
+    sumsq = c(0.322740606060606, 1.29093666666667, 1.61367727272727, 0.154497726190476,
+              1.03401541666667, 1.18851314285714, 0.125784226190476, 0.621541916666667,
               0.747326142857143, 0.198521625, 0.926316, 1.124837625),
-    meansq = c(0.161370303030303, 0.161367083333333, NA, 0.154497726190476, 
-               0.206803083333333, NA, 0.125784226190476, 0.124308383333333, 
+    meansq = c(0.161370303030303, 0.161367083333333, NA, 0.154497726190476,
+               0.206803083333333, NA, 0.125784226190476, 0.124308383333333,
                NA, 0.198521625, 0.154386, NA),
-    statistic = c(1.00001995262543, NA, NA, 0.747076512111044, NA, NA, 1.01187243223319, 
+    statistic = c(1.00001995262543, NA, NA, 0.747076512111044, NA, NA, 1.01187243223319,
                   NA, NA, 1.28587841514127, NA, NA),
-    r.squared = c(0.200003192407329, 0.799996807592671, 1, 0.129992442337717, 
-                  0.870007557662283, 1, 0.168312359192445, 0.831687640807555, 1, 
+    r.squared = c(0.200003192407329, 0.799996807592671, 1, 0.129992442337717,
+                  0.870007557662283, 1, 0.168312359192445, 0.831687640807555, 1,
                   0.17648913993253, 0.82351086006747, 1),
     p.value = c(0.3, NA, NA, 0.9, NA, NA, 0.4, NA, NA, 0.1, NA, NA),
     comparison = rep(c("all", "G1 - G2", "G1 - G3", "G2 - G3"), c(3,3,3,3)))
