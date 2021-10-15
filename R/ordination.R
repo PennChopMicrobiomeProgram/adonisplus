2268865d0e6d1827ce425b654c8d30a4c3559130
Kyle Bittinger <kylebittinger@gmail.com>
2021-10-15 12:26:00 -0400

    Add pcoaplus and plotting method for ordination

---
+++ R/ordination.R
@@ -0,0 +1,91 @@
+#' Principal coordinates analysis
+#'
+#' @param data A data frame giving information on the objects for the PCoA.
+#' @param distmat A matrix or \code{dist} object giving the distances between
+#'   objects in \code{data}. The distance matrix can contain distances between
+#'   additional objects not found in \code{data}, but if any of the objects in
+#'   \code{data} is missing, an error will occur.
+#' @param sample_id_var The column in \code{data} that gives the identifiers
+#'   for each row, used to find the matching distances in \code{distmat}.
+#' @param num_axes The number of PCoA axes to return in the output.
+#' @param x A tibble returned by \code{pcoaplus}.
+#' @param mapping An aesthetic mapping to use for the plot.
+#' @param ... Additional arguments are not used.
+#' @return For \code{pcoaplus}, a tibble with new columns giving the position
+#'   of each object along the principal coordinate axes. We add an additional
+#'   class to the resultant tibble (\code{"pcoaplus"}) to facilitate plotting.
+#'   The tibble also contains an attribute, "pctvar", which gives the percent
+#'   variation explained by each principal coordinate axis.
+#'
+#'   For \code{plot.pcoaplus}, a ggplot object.
+#' @export
+pcoaplus <- function (data, distmat, sample_id_var = SampleID, num_axes = 2) {
+  num_axes <- as.integer(num_axes)
+  stopifnot("num_axes must be 2 or more"= num_axes >= 2)
+  axis_numbers <- 1:num_axes
+  axis_names <- paste0("Axis.", axis_numbers)
+
+  sample_ids <- data %>%
+    dplyr::pull({{ sample_id_var }}) %>%
+    as.character()
+  stopifnot("Duplicated sample IDs" = anyDuplicated(sample_ids) == 0)
+
+  distmat <- usedist::dist_subset(distmat, sample_ids)
+  sample_id_var_name <- rlang::as_name(rlang::ensym(sample_id_var))
+  pcoa_obj <- ape::pcoa(distmat)
+  pcoa_df <- pcoa_obj$vectors %>%
+    `[`(sample_ids, axis_names) %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column(sample_id_var_name) %>%
+    tibble::as_tibble() %>%
+    dplyr::left_join(data, by = sample_id_var_name)
+
+  pctvar <- (pcoa_obj$values$Relative_eig * 100)[axis_numbers]
+  names(pctvar) <- axis_names
+  attr(pcoa_df, "pctvar") <- pctvar
+
+  class(pcoa_df) <- c("pcoaplus", class(pcoa_df))
+  pcoa_df
+}
+
+get_pcoa_axis <- function (qexp) {
+  val <- as.character(rlang::get_expr(qexp))
+  if (length(val) != 1) {
+    # Give up if we have something more complicated than a single value
+    return(NA_integer_)
+  }
+  m <- stringr::str_match(val, "^Axis\\.(\\d+)$")
+  axis_match <- m[1, 2]
+  # Will be NA if no match is found
+  as.integer(axis_match)
+}
+
+
+#' @describeIn pcoaplus Make a principal coordinates scatter plot
+#' @export
+plot.pcoaplus <- function (x,
+                           mapping = ggplot2::aes(x = Axis.1, y = Axis.2),
+                           ...) {
+  p <- x %>%
+    ggplot2::ggplot() +
+    ggplot2::geom_point(mapping) +
+    ggplot2::coord_equal()
+
+  pctvar <- attr(x, "pctvar")
+
+  x_axis_num <- get_pcoa_axis(mapping$x)
+  if (!is.na(x_axis_num)) {
+    x_pctvar <- round(pctvar[x_axis_num])
+    x_label <- stringr::str_glue("PCoA axis {x_axis_num} ({x_pctvar}%)")
+    p <- p + ggplot2::xlab(x_label)
+  }
+
+  y_axis_num <- get_pcoa_axis(mapping$y)
+  if (!is.na(y_axis_num)) {
+    y_pctvar <- round(pctvar[y_axis_num])
+    y_label <- stringr::str_glue("PCoA axis {y_axis_num} ({y_pctvar}%)")
+    p <- p + ggplot2::ylab(y_label)
+  }
+
+  p
+}
